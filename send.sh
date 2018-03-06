a24e7fed8c862896c6d459961a50af04d0c0d03a
Emmanuel Neri <emmanuelnerisouza@gmail.com>
2018-03-05 21:54:09 -0300

    Ajustando script de enviar dados para API
    - Fazendo requisicoes em paralelo, onde o curl faz o post em backgroud
    - Medindo tempo de envio

--- send.sh
+++ send.sh
@@ -4,7 +4,8 @@ url=http://localhost:8090
 
 value=100
 
-for i in {1..100}
+START_TIME=$SECONDS
+for i in {1..10}
 do
   cnpj="59683461000159"
   dateNow=$(date +"%FT%T")
@@ -12,6 +13,9 @@ do
 
   json="{\"identifier\":\"${i}\", \"customer\":{\"cnpj\":\"${cnpj}\"}, \"dateTime\":\"${dateNow}\",\"total\":$total}"
 
-  curl -d "${json}" -H "Content-Type: application/json" -X POST ${url}/orders/TEST
+  curl -d "${json}" -H "Content-Type: application/json" -X POST ${url}/orders/TEST &
 
 done
+
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "$(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec"
